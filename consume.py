66a61cb9af02b044785a7acfc5646a3c09bbbe92
QwertyJacob <jesusfcevallos@gmail.com>
2025-02-06 13:00:39 +0000

    refac

--- consume.py
+++ consume.py
@@ -66,15 +66,15 @@ def check_and_create_topics(topic_list):
     ]
 
     if topics_to_create:
-        logging.info(f"Creating missing topics: {[topic.topic for topic in topics_to_create]}")
+        logger.info(f"Creating missing topics: {[topic.topic for topic in topics_to_create]}")
         result = admin_client.create_topics(topics_to_create)
 
         for topic, future in result.items():
             try:
                 future.result()
-                logging.info(f"Topic '{topic}' created successfully.")
+                logger.info(f"Topic '{topic}' created successfully.")
             except KafkaException as e:
-                logging.error(f"Failed to create topic '{topic}': {e}")
+                logger.error(f"Failed to create topic '{topic}': {e}")
 
 
 def deserialize_message(msg):
@@ -90,10 +90,10 @@ def deserialize_message(msg):
     try:
         # Decode the message and deserialize it into a Python dictionary
         message_value = json.loads(msg.value().decode('utf-8'))
-        logging.debug(f"received message from topic [{msg.topic()}]")
+        print(f"received message from topic [{msg.topic()}]")
         return message_value
     except json.JSONDecodeError as e:
-        logging.error(f"Error deserializing message: {e}")
+        logger.error(f"Error deserializing message: {e}")
         return None
 
 

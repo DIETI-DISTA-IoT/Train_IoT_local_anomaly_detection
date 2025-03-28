ef2a96628a7451bd679cbd6dd0777551d3e27d7c
QwertyJacob <jesusfcevallos@gmail.com>
2025-03-28 17:56:28 +0000

    fix

--- consume.py
+++ consume.py
@@ -357,7 +357,7 @@ def train_model(**kwargs):
                     }
 
                 if mode == 'SW': 
-                    metrics_dict['mitigation_time'] = mitigation_times / len(mitigation_times)
+                    metrics_dict['mitigation_time'] = mitigation_times.sum() / len(mitigation_times)
                     metrics_dict['mitigation_reward'] = mitigation_reward
 
                 metrics_reporter.report(metrics_dict)
